1f34fa81ddecd98f1c469fb2cacd9b3223be79b7
jaisri25 <jaisri4101996@gmail.com>
2022-06-30 20:33:28 +0530

    file added

--- src/org/computer/Desktop.java
+++ src/org/computer/Desktop.java
@@ -3,10 +3,12 @@ package org.computer;
 public class Desktop extends LenovaComputer{
 	public void desktopSize() {
 System.out.println("desktop size is 44gb");
+System.out.println("jaya user");
 	}
 	public static void main(String[] args) {
 		Desktop d=new Desktop();
 		d.desktopSize();
 		d.computerModel();
+		
 	}
 }
