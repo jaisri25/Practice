90ccd04745a9890351bbfa598d03ecdc73bb3b57
jaisri25 <jaisri4101996@gmail.com>
2022-08-08 21:02:34 +0530

    project

--- src/org/computer/Desktop.java
+++ src/org/computer/Desktop.java
@@ -4,6 +4,7 @@ public class Desktop extends LenovaComputer{
 	public void desktopSize() {
 System.out.println("desktop size is 44gb");
 System.out.println("jaya user");
+System.out.println("jaya is practicing");
 	}
 	public static void main(String[] args) {
 		Desktop d=new Desktop();
